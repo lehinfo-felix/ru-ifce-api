d96eb82c8bb62359d13f0af39798f7838bd7bacd
Carlos Emannoel <carlosemannoel2019@gmail.com>
2022-11-22 15:01:10 -0300

    consultas retornando os dados

--- src/presetation/scripts/consultas/consultaAlmoco.ts
+++ src/presetation/scripts/consultas/consultaAlmoco.ts
@@ -1,48 +1,42 @@
 import puppeteer from "puppeteer";
 export const getRefeicoes = async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+  c
 
-  await page.goto("https://ru.cedro.ifce.edu.br/sign-in");
-  await page.type('[name="email"]', "lehinfo.felix@gmail.com");
-  await page.type('[name="password"]', "123456");
-  await page.click(".App1-MuiButton-label");
-  await page.waitForNavigation();
 
-  const length = await page.evaluate(() => {
-    return document.querySelectorAll(".App1-MuiCardContent-root")[1].children
-      .length;
-  });
 
-  const arrayPosition: Array<number> = [];
-  for (let index = 1; index < length - 2; index++) {
-    const indexArray = length - index;
-    arrayPosition.push(indexArray);
-  }
-  console.log(arrayPosition);
-//setaId
-  await page.evaluate(() => {
-    return (document.querySelectorAll(".App1-MuiCardContent-root")[1].id =
-      "refeicoes");
-  });
+  //  setTimeout(async () => {
+  //   refeicao = await pegarRefeicao();
+  // }, 1000);
 
-  const text = page.evaluate(() => {
-    //return document.querySelector("#refeicoes")?.textContent
+  // while (loop){
+  //   console.log('No While')
+  //   if(pegouRefeicao){
+  //     console.log(refeicao)
+  //     loop = false
+  //   }
 
-    let title : any =  document.querySelector("#refeicoes > p")?.innerHTML
+  // }
 
-    let refeicaoUm: string = document.querySelectorAll("#refeicoes > div")[0]?.innerHTML
-    let refeicaoDois: any = document.querySelectorAll("#refeicoes > div")[1]?.innerHTML
-    let refeicaoTres: any = document.querySelectorAll("#refeicoes > div")[2]?.innerHTML
-    let refeicaoQuatro: any = document.querySelectorAll("#refeicoes > div")[3]?.innerHTML
+  // const text1 = await pageStudent.$eval('#refeicoes > div ', (text)=>text.innerHTML)
+  // console.log( text1)
 
-    let descart : string = `<p class="\App1-MuiTypography-root jss11 App1-MuiTypography-body1 App1-MuiTypography-colorTextPrimary\">`
-// <p class="App1-MuiTypography-root jss11 App1-MuiTypography-body1 App1-MuiTypography-colorTextPrimary">
-    let refeicaoUmReplaced : string = refeicaoUm.replace(descart, '')
+  //   const text = page.evaluate(() => {
+  //     //return document.querySelector("#refeicoes")?.textContent
 
-    let refeicaoUmSplited : string = refeicaoUmReplaced.split('</p>').join('')
+  //     let title : any =  document.querySelector("#refeicoes > p")?.innerHTML
 
-    return refeicaoUmSplited
-  });
-return text
+  //     let refeicaoUm: string = document.querySelectorAll("#refeicoes > div")[0]?.innerHTML
+  //     let refeicaoDois: any = document.querySelectorAll("#refeicoes > div")[1]?.innerHTML
+  //     let refeicaoTres: any = document.querySelectorAll("#refeicoes > div")[2]?.innerHTML
+  //     let refeicaoQuatro: any = document.querySelectorAll("#refeicoes > div")[3]?.innerHTML
+
+  //     let descart : string = `<p class="\App1-MuiTypography-root jss11 App1-MuiTypography-body1 App1-MuiTypography-colorTextPrimary\">`
+  // // <p class="App1-MuiTypography-root jss11 App1-MuiTypography-body1 App1-MuiTypography-colorTextPrimary">
+  //     let refeicaoUmReplaced : string = refeicaoUm.replace(descart, '')
+
+  //     let refeicaoUmSplited : string = refeicaoUmReplaced.split('</p>').join('')
+
+  //     return refeicaoUmSplited
+  //});
+  //return text
 };
