3ac8b3fcb42cb248480361f550f4a121c5bb5734
Leandro Félix <58048297+lehinfo-felix@users.noreply.github.com>
2022-12-16 09:55:00 -0300

    fix: correção dos nomes das rotas

--- src/main/routes/consultas-router.ts
+++ src/main/routes/consultas-router.ts
@@ -6,7 +6,7 @@ import { Router } from 'express'
 export default (router: Router): void => {
   router.get('/', () => console.log('pong'))
   router.get('/consulta/almoco', AdpterRouter(makeConsultaAlmocoController()))
-  router.get('/consulta/lanche/manha', AdpterRouter(makeConsultaLancheManhaController()))
-  router.get('/consulta/lanhe/tarde', AdpterRouter(makeConsultaLancheTardeContrller()))
-  router.get('/consulta/lanche/noite', AdpterRouter(makeConsultaLancheNoiteController()))
+  router.get('/consulta/lanche-manha', AdpterRouter(makeConsultaLancheManhaController()))
+  router.get('/consulta/lanche-tarde', AdpterRouter(makeConsultaLancheTardeContrller()))
+  router.get('/consulta/lanche-noite', AdpterRouter(makeConsultaLancheNoiteController()))
 }
